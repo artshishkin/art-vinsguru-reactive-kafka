ee196bcc24361feb84273def78797ed54ff8a22a
Art <d.art.shishkin@gmail.com>
2025-01-07 00:43:08 +0200

    41. Creating Kafka Receiver (#2)

--- reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec01/Lec01KafkaConsumer.java
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec01/Lec01KafkaConsumer.java
@@ -1,27 +1,34 @@
 package net.shyshkin.study.kafkareactor.playground.sec01;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.kafka.listener.ConsumerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 
 public class Lec01KafkaConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(Lec01KafkaConsumer.class);
+
     public static void main(String[] args) {
-        Map<String, Object> consumerProperties = Map.of(
-                BOOTSTRAP_SERVERS_CONFIG,"localhost:9092",
+        var consumerProperties = Map.<String, Object>of(
+                BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 GROUP_ID_CONFIG, "demo-group"
         );
-//        var receiverOptions = ReceiverOptions.create(consumerProperties);
+        var receiverOptions = ReceiverOptions.create(consumerProperties)
+                .subscription(List.of("order-events")); //topic name
 
-//        KafkaReceiver<Object, Object> kafkaReceiver = KafkaReceiver.create(receiverOptions);
+        KafkaReceiver<Object, Object> kafkaReceiver = KafkaReceiver.create(receiverOptions);
+        kafkaReceiver.receive()
+                .doOnNext(record -> log.info("key: {}, value: {}", record.key(), record.value()))
+                .subscribe();
     }
 
 }
