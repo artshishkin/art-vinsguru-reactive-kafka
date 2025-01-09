13e54fa6b31ce7d1ddb4f8c8c3e9451517096b7d
Art <d.art.shishkin@gmail.com>
2025-01-09 15:52:05 +0200

    102. Poison Pill Messages - Fix (#6)

--- reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec14/S14KafkaConsumer.java
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec14/S14KafkaConsumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
@@ -20,19 +21,29 @@ public class S14KafkaConsumer {
         var consumerProperties = Map.<String, Object>of(
                 BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+//                VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
                 GROUP_ID_CONFIG, "demo-group",
                 AUTO_OFFSET_RESET_CONFIG, "earliest",
                 GROUP_INSTANCE_ID_CONFIG, "1"
         );
-        var receiverOptions = ReceiverOptions.create(consumerProperties)
+        var receiverOptions = ReceiverOptions.<String, Integer>create(consumerProperties)
+                .withValueDeserializer(errorHandlingDeserializer())
                 .subscription(List.of("order-events")); //topic name
 
-        KafkaReceiver<Object, Object> kafkaReceiver = KafkaReceiver.create(receiverOptions);
+        KafkaReceiver<String, Integer> kafkaReceiver = KafkaReceiver.create(receiverOptions);
         kafkaReceiver.receive()
                 .doOnNext(record -> log.info("key: {}, value: {}", record.key(), record.value()))
                 .doOnNext(record -> record.receiverOffset().acknowledge())
                 .subscribe();
+    }
+
+    private static ErrorHandlingDeserializer<Integer> errorHandlingDeserializer() {
+        ErrorHandlingDeserializer<Integer> deserializer = new ErrorHandlingDeserializer<>(new IntegerDeserializer());
+        deserializer.setFailedDeserializationFunction(info -> {
+            log.error("failed record: {} in topic `{}`", new String(info.getData()), info.getTopic());
+            return -10_000;
+        });
+        return deserializer;
     }
 
 }
