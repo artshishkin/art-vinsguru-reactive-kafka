a141a31d67990c4b2c048d13e40017987815e3f4
Art <d.art.shishkin@gmail.com>
2025-01-09 15:28:36 +0200

    101. Poison Pill Messages (#6)

---
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec14/S14KafkaConsumer.java
@@ -0,0 +1,38 @@
+package net.shyshkin.study.kafkareactor.playground.sec14;
+
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+public class S14KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(S14KafkaConsumer.class);
+
+    public static void main(String[] args) {
+        var consumerProperties = Map.<String, Object>of(
+                BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+                GROUP_ID_CONFIG, "demo-group",
+                AUTO_OFFSET_RESET_CONFIG, "earliest",
+                GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+        var receiverOptions = ReceiverOptions.create(consumerProperties)
+                .subscription(List.of("order-events")); //topic name
+
+        KafkaReceiver<Object, Object> kafkaReceiver = KafkaReceiver.create(receiverOptions);
+        kafkaReceiver.receive()
+                .doOnNext(record -> log.info("key: {}, value: {}", record.key(), record.value()))
+                .doOnNext(record -> record.receiverOffset().acknowledge())
+                .subscribe();
+    }
+
+}
---
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec14/S14KafkaProducer.java
@@ -0,0 +1,45 @@
+package net.shyshkin.study.kafkareactor.playground.sec14;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class S14KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(S14KafkaProducer.class);
+
+    public static void main(String[] args) {
+
+        var producerProperties = Map.<String, Object>of(
+                BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class
+        );
+
+        var senderOptions = SenderOptions.<String, Integer>create(producerProperties);
+
+        var eventFlux = Flux.range(1,100)
+                .map(i -> new ProducerRecord<>("order-events", i.toString(), i))
+                .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        KafkaSender<String, Integer> sender = KafkaSender.create(senderOptions);
+        sender
+                .send(eventFlux)
+                .doOnNext(result -> log.info("correlation id: {}", result.correlationMetadata()))
+                .doOnComplete(sender::close)
+                .subscribe();
+
+
+    }
+
+}
