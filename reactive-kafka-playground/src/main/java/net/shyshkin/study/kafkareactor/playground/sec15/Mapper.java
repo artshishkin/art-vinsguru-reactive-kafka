8868555bf4aa8b0541f3814d16433072a4a01d88
Art <d.art.shishkin@gmail.com>
2025-01-10 16:20:47 +0200

    109. Transfer Event Processor (#7)

--- reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec15/Mapper.java
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec15/Mapper.java
@@ -1,8 +1,15 @@
 package net.shyshkin.study.kafkareactor.playground.sec15;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import reactor.kafka.receiver.ReceiverRecord;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Mapper {
+
+    public static final String TOPIC = "transaction-events";
 
     //1:a,b,100
     //key:from,to,amount
@@ -23,6 +30,15 @@ public class Mapper {
                 Integer.parseInt(arr[2]),
                 action
         );
+    }
+
+    public List<SenderRecord<String, String, String>> toSenderRecords(TransferEvent event) {
+        return Stream.of(
+                        new ProducerRecord<>(TOPIC, event.key(), "%s,%d".formatted(event.from(), -event.amount())),
+                        new ProducerRecord<>(TOPIC, event.key(), "%s,%d".formatted(event.to(), event.amount()))
+                )
+                .map(pr -> SenderRecord.create(pr, pr.key()))
+                .toList();
     }
 
     private Runnable ack(ReceiverRecord<String, String> record) {
--- reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec15/TransferEventProcessor.java
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec15/TransferEventProcessor.java
@@ -1,4 +1,57 @@
 package net.shyshkin.study.kafkareactor.playground.sec15;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderResult;
+import reactor.kafka.sender.TransactionManager;
+
+import java.util.function.Predicate;
+
 public class TransferEventProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(TransferEventProcessor.class);
+
+    private final KafkaSender<String, String> sender;
+    private final Mapper mapper;
+
+    public TransferEventProcessor(KafkaSender<String, String> sender, Mapper mapper) {
+        this.sender = sender;
+        this.mapper = mapper;
+    }
+
+    public Flux<SenderResult<String>> process(Flux<TransferEvent> events) {
+        return events
+                .concatMap(this::validate)
+                .concatMap(this::sendTransaction);
+    }
+
+    private Mono<SenderResult<String>> sendTransaction(TransferEvent event) {
+        var senderRecords = mapper.toSenderRecords(event);
+        var recordFlux = Flux.fromIterable(senderRecords);
+        TransactionManager manager = this.sender.transactionManager();
+
+        return manager.begin()
+                .then(sender.send(recordFlux)
+                        .concatWith(Mono.fromRunnable(event.acknowledge()))
+                        .concatWith(manager.commit())
+                        .last() //for simplicity. It is recommended to .collectList -> analyze -> make own consolidated result
+                )
+                .doOnError(ex -> log.error(ex.getMessage()))
+                .onErrorResume(ex -> manager.abort());
+    }
+
+    // simulate situation
+    // if key=5 account does not have enough money
+    private Mono<TransferEvent> validate(TransferEvent event) {
+        return Mono.just(event)
+                .filter(Predicate.not(e -> "5".equals(e.key())))
+                .switchIfEmpty(
+                        Mono.<TransferEvent>fromRunnable(event.acknowledge())
+                                .doFirst(() -> log.info("fails validation: {}", event))
+                );
+    }
+
 }
