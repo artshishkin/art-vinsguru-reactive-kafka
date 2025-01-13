d3d556e73e830ec6b75dc4acdde643ee19446b0a
Art <d.art.shishkin@gmail.com>
2025-01-13 14:51:03 +0200

    120. Simple Spring Kafka Consumer (#8)

---
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec16/ConsumerRunner.java
@@ -0,0 +1,24 @@
+package net.shyshkin.study.kafkareactor.playground.sec16;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ConsumerRunner implements CommandLineRunner {
+
+    private final ReactiveKafkaConsumerTemplate<String, String> consumerTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        consumerTemplate
+                .receive()
+                .doOnNext(record -> log.info("key: {}, value: {}", record.key(), record.value()))
+                .subscribe();
+    }
+
+}
