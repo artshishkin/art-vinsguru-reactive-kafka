e71823947998bad23dd9b752bb5920034cbde2c2
Art <d.art.shishkin@gmail.com>
2025-01-13 12:12:15 +0200

    119. Introduction (Section 10: Reactive Kafka With Spring #8)

---
+++ reactive-kafka-playground/src/main/java/net/shyshkin/study/kafkareactor/playground/sec16/KafkaConsumerConfig.java
@@ -0,0 +1,25 @@
+package net.shyshkin.study.kafkareactor.playground.sec16;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ReceiverOptions<String, String> receiverOptions(KafkaProperties kafkaProperties) {
+        return ReceiverOptions.<String, String>create(kafkaProperties.buildConsumerProperties())
+                .subscription(List.of("order-events"));
+    }
+
+    @Bean
+    public ReactiveKafkaConsumerTemplate<String, String> consumerTemplate(ReceiverOptions<String, String> receiverOptions) {
+        return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
+    }
+
+}
